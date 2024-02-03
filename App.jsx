5c8cd5868e255098a446be6ba9705781fc6f6704
jox <zincir_890@hotmail.com>
2024-02-03 17:03:15 +0300

    Changed Style and Added Routes

--- App.jsx
+++ App.jsx
@@ -7,59 +7,54 @@ import "react-native-gesture-handler";
 import HomeScreen from "./src/screens/HomeScreen";
 import LikedScreen from "./src/screens/LikedScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import Appbar from './src/components/Appbar';
+import Appbar from "./src/components/Appbar";
 import PostDetails from "./src/components/PostDetails";
+import SettingsScreen from "./src/screens/SettingsScreen";
+import DonateScreen from "./src/screens/DonateScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const TabNavigator = () => {
   return (
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{ tabBarActiveTintColor: "#e91e63", tabBarStyle: { borderTopStartRadius: 15, borderTopEndRadius: 15 } }}
-        style={styles.nav}
-        
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-            headerShown: false
-          }}
-        />
-        <Tab.Screen
-          name="Liked"
-          component={LikedScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name='heart'
-                color={color}
-                size={size}
-              />
-            ),
-            headerShown: false
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="account"
-                color={color}
-                size={size}
-              />
-            ),
-            headerShown: false
-          }}
-        />
-      </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "#e91e63",
+        tabBarStyle: { borderTopStartRadius: 15, borderTopEndRadius: 15 },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Liked"
+        component={LikedScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="heart" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
@@ -67,25 +62,35 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
+        <Stack.Screen
           name="TabNavigator"
           component={TabNavigator}
-          options={{headerShown: false}}
+          options={styles.header}
         />
         <Stack.Screen
           name="PostDetails"
           component={PostDetails}
-          options={{headerShown: false}}
+          options={styles.header}
         />
         <Stack.Screen
           name="Appbar"
           component={Appbar}
-          options={{headerShown: false}}
+          options={styles.header}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={styles.header}
+        />
+        <Stack.Screen
+          name="Donate"
+          component={DonateScreen}
+          options={styles.header}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -103,6 +108,9 @@ const styles = StyleSheet.create({
     // alignSelf: 'center',
     // display: 'flex'
   },
+  header: {
+    headerShown: false
+  }
 });
 
 export default App;
