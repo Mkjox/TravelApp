2c30321bafddd01ef09ebb3941404345440d3d12
jox <zincir_890@hotmail.com>
2024-02-13 17:52:05 +0300

    Deleted unnecessary lines and Changed styles

--- src/screens/LikedScreen.jsx
+++ src/screens/LikedScreen.jsx
@@ -9,9 +9,10 @@ import {
 } from "react-native";
 import { Card } from "react-native-paper";
 import Feather from "@expo/vector-icons/Feather";
-import Entypo from "@expo/vector-icons/Entypo";
+import { MaterialIcons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LikedData from "../assets/data/likedData.json";
+import colors from "../assets/colors/colors";
 
 const LikedScreen = ({ navigation }) => {
   const [data, setData] = useState([]);
@@ -26,38 +27,38 @@ const LikedScreen = ({ navigation }) => {
         <View style={styles.menuWrapper}>
           <Feather name="menu" size={32} style={styles.menuButton} />
         </View>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("PostDetails", { item: item })}
-        >
-          <View style={styles.galleryWrapper}>
-            <View style={styles.galleryItemWrapper}>
-              <FlatList
-                data={data}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                  <TouchableOpacity>
-                    <Card
-                      style={styles.postInnerWrapper}
-                      onPress={() =>
-                        navigation.navigate("PostDetails", { item: item })
-                      }
-                    >
-                      <Card.Cover
-                        source={{ uri: "https://picsum.photos/700" }}
-                      />
-                      <Card.Content style={styles.galleryItemText}>
-                        <Text>{item.title}</Text>
-                        <Text>{item.body}</Text>
-                      </Card.Content>
-                    </Card>
-                  </TouchableOpacity>
-                )}
-                alwaysBounceVertical={true}
-                showsVerticalScrollIndicator={false}
-              />
-            </View>
+        <View style={styles.likedWrapper}>
+          <View style={styles.likedItemWrapper}>
+            <FlatList
+              data={data}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity>
+                  <Card
+                    style={styles.likedPostInnerWrapper}
+                    onPress={() =>
+                      navigation.navigate("PostDetails", { item: item })
+                    }
+                  >
+                    <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+                    <Card.Content style={styles.likedItemText}>
+                      <Text style={styles.likedItemTitle}>
+                      {item.title}
+                      </Text>
+                      <Text style={styles.likedItemPlace}>
+                      <MaterialIcons name="place" size={15} color={"black"}/>
+                      {item.place}
+                      </Text>
+                      <Text>{item.body}</Text>
+                    </Card.Content>
+                  </Card>
+                </TouchableOpacity>
+              )}
+              alwaysBounceVertical={true}
+              showsVerticalScrollIndicator={false}
+            />
           </View>
-        </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -79,11 +80,11 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "left",
   },
-  galleryWrapper: {
+  likedWrapper: {
     marginHorizontal: 5,
     marginBottom: 10,
   },
-  galleryItem: {
+  likedItem: {
     width: 300,
     height: 120,
     justifyContent: "flex-end",
@@ -91,36 +92,45 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginVertical: 10,
   },
-  galleryItemImage: {
+  likedItemImage: {
     borderRadius: 20,
   },
-  galleryItemText: {
+  likedItemText: {
     fontWeight: "400",
     fontSize: 16,
     color: "#FBFCFE",
     marginTop: 10,
     paddingBottom: 10,
   },
-  galleryItemLocationWrapper: {
+  likedItemTitle: {
+    marginBottom: 7.5,
+    fontSize: 16,
+    color: colors.black
+  },
+  likedItemPlace: {
+    marginBottom: 7.5,
+    color: colors.darkGray
+  },
+  likedItemLocationWrapper: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 5,
     marginLeft: 5,
   },
-  galleryItemLocationText: {
+  likedItemLocationText: {
     marginLeft: 5,
     fontWeight: "400",
     fontSize: 15,
     color: "#FBFCFE",
     marginBottom: 5,
   },
-  galleryItemWrapper: {
+  likedItemWrapper: {
     paddingVertical: 20,
     marginBottom: 10,
     marginHorizontal: 5,
     width: "auto",
   },
-  postInnerWrapper: {
+  likedPostInnerWrapper: {
     marginBottom: 5,
   },
 });
