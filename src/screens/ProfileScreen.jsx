c6d58a436a67c18a3aba251e0ae0ed034720eb5c
jox <zincir_890@hotmail.com>
2024-02-29 02:13:39 +0300

    Added some styles

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -6,6 +6,7 @@ import { Avatar, Caption, Title, TouchableRipple } from "react-native-paper";
 import { CardTitle } from "@rneui/base/dist/Card/Card.Title";
 import { Entypo, FontAwesome } from '@expo/vector-icons';
 import LikedData from '../assets/data/likedData.json';
+import colors from "../assets/colors/colors";
 
 
 const ProfileScreen = () => {
@@ -21,41 +22,40 @@ const ProfileScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <View style={styles.profileTopWrapper}>
-          <Avatar.Image size={70} source={"https://picsum.photos/400"} />
-          <Text>@</Text>
-          <Entypo name="location-pin" size={18}>
-            <Text>Location</Text>
-          </Entypo>
-          <Entypo name="phone" size={18}>
-            <Text>Phone Number</Text>
-          </Entypo>
-          <Entypo name="mail" size={18}>
-            <Text>Email</Text>
-          </Entypo>
-        </View>
-        <View style={styles.CountWrapper}>
-            <Text>Count</Text>
-            <Caption>Post Count</Caption>
-        </View>
-        <View style={styles.options}>
-          <FontAwesome name="heart">
-            <Text>Liked Posts</Text>
-          </FontAwesome>
-          <FontAwesome name="comment">
-            <Text>Comments</Text>
-          </FontAwesome>
-          <FontAwesome name="pencil">
-            <Text>Customize Profile</Text>
-          </FontAwesome>
-          <FontAwesome name="share">
-            <Text>Recommend the app to your friend</Text>
-          </FontAwesome>
-          <FontAwesome name="gear">
-            <Text>Settings</Text>
-          </FontAwesome>
-        </View>
+      <View style={styles.profileTopWrapper}>
+        <Avatar.Image size={70} style={styles.avatar} />
+        <Text style={styles.headerText}>Welcome</Text>
+        <Text style={[styles.headerText, { marginBottom: 5 }]}>@</Text>
+        <Entypo name="location-pin" size={18} color={'#F5F5F5'} style={styles.profileDetails}>
+          <Text style={styles.profileDetailsText}>Location</Text>
+        </Entypo>
+        <Entypo name="phone" size={18} color={'#F5F5F5'} style={styles.profileDetails}>
+          <Text style={styles.profileDetailsText}>Phone Number</Text>
+        </Entypo>
+        <Entypo name="mail" size={18} color={'#F5F5F5'} style={styles.profileDetails}>
+          <Text style={styles.profileDetailsText}>Email</Text>
+        </Entypo>
+      </View>
+      <View style={styles.countWrapper}>
+        <Text>Count</Text>
+        <Caption>Post Count</Caption>
+      </View>
+      <View style={styles.options}>
+        <FontAwesome name="heart">
+          <Text>Liked Posts</Text>
+        </FontAwesome>
+        <FontAwesome name="comment">
+          <Text>Comments</Text>
+        </FontAwesome>
+        <FontAwesome name="pencil">
+          <Text>Customize Profile</Text>
+        </FontAwesome>
+        <FontAwesome name="share">
+          <Text>Recommend the app to your friend</Text>
+        </FontAwesome>
+        <FontAwesome name="gear">
+          <Text>Settings</Text>
+        </FontAwesome>
       </View>
     </SafeAreaView>
   );
@@ -65,7 +65,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-
+  profileTopWrapper: {
+    backgroundColor: '#3CB2D8',
+    borderBottomEndRadius: 20,
+    borderBottomLeftRadius: 20,
+    height: 300,
+    alignItems: 'center',
+    shadowOpacity: 30,
+  },
+  information: {
+  },
+  headerText: {
+    fontWeight: '800',
+    color: colors.white,
+    fontSize: 18
+  },
+  avatar: {
+    marginTop: 15,
+    marginBottom: 10
+  },
+  profileDetails: {
+    marginVertical: 5,
+  },
+  profileDetailsText: {
+    fontWeight: '400',
+  }
 });
 
 export default ProfileScreen;
